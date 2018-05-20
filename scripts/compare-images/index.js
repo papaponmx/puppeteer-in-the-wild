60ecec3c2ed4b7e2215d18f67cca3e77bf9d8790
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-05-20 16:54:28 -0500

    Removed console folder and added functionality.

--- scripts/compare-images/index.js
+++ scripts/compare-images/index.js
@@ -1,12 +1,36 @@
 const fs = require('fs'); // Nodes File System
 const puppeteer = require('puppeteer'); // High level API to interact with headless Chrome
-const { logSuccess } = require('../../console');
-// import  { logSuccess } from '../../console'; // Functions that allow us to write cooler console logs.
+const signale = require('signale');
 // import LOCATORS from './locators'; // A JSON with all the CSS locators we need.
-// import { logSuccess } from '../../console';
-
+const config = require('./config');
 
 // 1. Open browser.
-logSuccess('It works');
+signale.debug('Opening browser...');
 
-// console.log('It works');
+const testToRun = async (params) => {
+
+
+
+  console.log(JSON.stringify(config));
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const d = new Date();
+  const name = `SOME_INPUT_FROM_TESTING_SCRIPT_${d.getDate()}_${d.getHours()}h${d.getMinutes()}`;
+
+  await signale.watch('Navigating to the site');
+
+
+
+  await signale.watch('Filling up the form');
+
+
+
+  await signale.success('Everything is working as expected');
+
+  await browser.close();
+
+};
+
+
+testToRun();
