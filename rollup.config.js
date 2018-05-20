a3a8efb336f6d03dfce6dab9a986edd3eb0ed6ce
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-05-20 15:15:35 -0500

    Added multiple entries for rollup.

--- rollup.config.js
+++ rollup.config.js
@@ -5,10 +5,21 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify';
+import multiEntry from "rollup-plugin-multi-entry";
 
 export default {
-  entry: 'scripts/compare-images/index.js',
-  dest: 'build/js/main.min.js',
+  entry: [
+    'scripts/compare-images',
+    'scripts/add-campaign',
+    'scripts/open-broser',
+    'scripts/get-testing-url',
+  ],
+  dest: [
+    'build/compare-images.min.js',
+    'build/add-campaign.min.js',
+    'build/open-broser.min.js',
+    'build/get-testing-url.min.js',
+  ],
   format: 'iife',
   sourceMap: 'inline',
   plugins: [
@@ -31,5 +42,6 @@ export default {
       ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
     }),
     (process.env.NODE_ENV === 'production' && uglify()),
+    multiEntry(),
   ],
 };
