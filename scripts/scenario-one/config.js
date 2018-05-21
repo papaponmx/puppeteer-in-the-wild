63a533252d23a0e88b53abc82136eb01b2c6df38
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-05-20 20:33:11 -0500

    Final corrections.

---
+++ scripts/scenario-one/config.js
@@ -0,0 +1,9 @@
+// JSON with url with all opts and shit.
+const config =  {
+  url: 'http://localhost:3000/',
+  phoneNumber: '123-456-7890',
+  email: 'test@example.com',
+  password: 'test1234',
+}
+
+module.exports.config = config;
---
+++ scripts/scenario-one/index.js
@@ -0,0 +1,35 @@
+const fs = require('fs'); // Nodes File System
+const puppeteer = require('puppeteer'); // High level API to interact with headless Chrome
+const signale = require('signale');
+// import LOCATORS from './locators'; // A JSON with all the CSS locators we need.
+const config = require('./config');
+
+// 1. Open browser.
+
+const runTest = async (params) => {
+  signale.debug('Opening browser...');
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const d = new Date();
+  const dateString = `${d.getDate()}_${d.getHours()}h${d.getMinutes()}`;
+  const userName = `USER_FROM_TESTING_SCRIPT_${dateString}`;
+
+  // 1. Go to the website;
+  await signale.watch('Navigating to the site');
+  await page.goto(LOCAL_HOST_URL);
+
+
+
+
+  await signale.watch('Filling up the form');
+
+
+
+  await signale.success('Everything is working as expected');
+
+  await browser.close();
+
+};
+
+
+runTest();
