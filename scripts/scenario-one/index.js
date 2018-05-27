88d596acb96b1a1f2ee4321fc2600037244adfac
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-05-26 19:12:16 -0500

    Fixed import and added emojis.

--- scripts/scenario-one/index.js
+++ scripts/scenario-one/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs'); // Nodes File System
 const puppeteer = require('puppeteer'); // High level API to interact with headless Chrome
 const signale = require('signale');
-// import LOCATORS from './locators'; // A JSON with all the CSS locators we need.
+const locators = require('./locators'); // A JSON with all the CSS locators we need.
 const config = require('./config');
 
 // 1. Open browser.
@@ -14,21 +14,12 @@ const runTest = async (params) => {
   const dateString = `${d.getDate()}_${d.getHours()}h${d.getMinutes()}`;
   const userName = `USER_FROM_TESTING_SCRIPT_${dateString}`;
 
-  // 1. Go to the website;
-  await signale.watch('Navigating to the site');
+  // 2. Go to the website;
+  await signale.watch('Navigating to the site 🚢');
   await page.goto(LOCAL_HOST_URL);
-
-
-
-
-  await signale.watch('Filling up the form');
-
-
-
-  await signale.success('Everything is working as expected');
-
+  await signale.watch('Filling up the form 🙌');
+  await signale.success('Everything is working as expected ✅');
   await browser.close();
-
 };
 
 
